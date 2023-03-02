75f1d6c51e62b55ee49e0bc2dbbf5bd2197dbd05
Chaim Fishman <csfwny@gmail.com>
2023-03-02 17:01:37 +0200

    Point class method imlementations subtract is not imeplemented yet

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -21,15 +21,27 @@ public class Point {
     }
 
     public Point add(Vector v) {
-
+        return new Point(xyz.add(v.xyz));
     }
 
-    public double distance(Point p) {
-
+    /***
+     *calculates the distance between this point and point param
+     * @param point other point of which we want distance to be calculated to.
+     * @return this method returns a double which is the distance between the two points
+     */
+    public double distance(Point point) {
+        return Math.sqrt(this.distanceSquared(point));
     }
 
-    public double distanceSquared(Point p) {
-
+    /***
+     *
+     * @param point
+     * @return
+     */
+    public double distanceSquared(Point point) {
+        Double3 x = point.xyz.subtract(xyz);
+        Double3 squared = x.product(x);
+        return squared.d1+squared.d2+squared.d3;
     }
 
 }
