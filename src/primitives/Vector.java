99e88dc46974e31e356816e79193ae965827a515
Avi-W <avi.weiner12@gmail.com>
2023-03-02 17:02:31 +0200

    Worked on Vector

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,13 +1,35 @@
 package primitives;
 
 public class Vector extends Point{
+    /**
+     * Cronstructs a vector
+     * @param x coordinate
+     * @param y coordinate
+     * @param z coordinate
+     */
     public Vector(double x, double y, double z) {
         super(x, y, z);
+        if (this.xyz.equals(Double3.ZERO))
+            throw new IllegalArgumentException("Argument can't equal zero vector");
+
     }
 
-    public Vector(Double3 x) {
-        super(x);
+    /**
+     *
+     * @param xyz
+     */
+    public Vector(Double3 xyz) {
+        super(xyz);
+        if (xyz.equals(Double3.ZERO))
+            throw new IllegalArgumentException("Argument can't equal zero vector");
     }
 
+    /**
+     * Performs addition between two vectors and returns the result as a vector.
+     * @param vector
+     */
+    public add(Vector vector) {
+
+    }
 
 }
