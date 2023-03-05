2e35016f567c7ad8de8401e50198b9f31a0b4459
Chaim Fishman <csfwny@gmail.com>
2023-03-05 18:53:39 +0200

    creeated Ray class and constructor which receives a point and vector

---
+++ src/primitives/Ray.java
@@ -0,0 +1,10 @@
+package primitives;
+
+public class Ray {
+    Point point;
+    Vector vector;
+    public Ray(Point point, Vector vector){
+        this.point = point;
+        this.vector = vector;
+    }
+}
