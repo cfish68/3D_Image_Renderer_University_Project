940836982750c49f1b9bb1ed7121ecced63e6c57
Chaim Fishman <csfwny@gmail.com>
2023-03-23 15:36:35 +0200

    added unitests for point

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,28 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void subtract() {//assertEquals() (expected, actual, message "errorrorro");
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void distance() {
+    }
+
+    @Test
+    void distanceSquared() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
