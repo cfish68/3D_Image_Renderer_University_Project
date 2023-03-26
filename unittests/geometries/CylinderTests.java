3c104f6eb2617f083c7914d9a1260e489f1a899a
Avi-W <avi.weiner12@gmail.com>
2023-03-26 17:46:35 +0300

    Half done CylinderTests

--- unittests/geometries/CylinderTests.java
+++ unittests/geometries/CylinderTests.java
@@ -2,6 +2,8 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
  * Unit tests for geometries.Cylinder class
  */
 class CylinderTests {
+
+    /**
+     * Test method for {@link geometries.Cylinder#Cylinder(double, Ray, double)}
+     */
+    public void testConstructor() {
+        // ============ Equivalence Partitions Tests ==============
+
+        //TC01: Correct Cylinder
+        try {
+            new Cylinder(3, new Ray(new Point(0,0,0), new Vector(1,1,1)), 3);
+        } catch (IllegalArgumentException e) {
+            fail("Failed constructing a correct Cylinder");
+        }
+
+        //TC02: Negative radius
+        assertThrows(IllegalArgumentException.class,
+                () -> new Cylinder(-3, new Ray(new Point(0,0,0), new Vector(1,1,1)), 3),
+                "Constructed cylinder with negative radius");
+
+        //TC03: Negative height
+        assertThrows(IllegalArgumentException.class,
+                () -> new Cylinder(3, new Ray(new Point(0,0,0), new Vector(1,1,1)), -3),
+                "Constructed cylinder with negative height");
+
+        // =============== Boundary Values Tests ==================
+
+        //TC11:
+
+    }
 
     /**
      * Test method for {@link geometries.Cylinder#getNormal(primitives.Point)}
