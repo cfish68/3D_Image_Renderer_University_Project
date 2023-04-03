d029172ff43993548915c189ef4ae5efaba031e0
Chaim Fishman <csfwny@gmail.com>
2023-04-03 08:14:44 +0300

    added Intersectable interface. added comments to intersectable. changed Geometry interface to extend Intersectable

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -6,6 +6,6 @@ import primitives.Point;
  * this interface is the "mother" of all geomentries it forces all geometries to
  * implement the getNormal method.
  */
-public interface Geometry {
+public interface Geometry extends Intersectable {
     public Vector getNormal(Point point);
 }
---
+++ src/geometries/Intersectable.java
@@ -0,0 +1,17 @@
+package geometries;
+import primitives.Point;
+import primitives.Ray;
+
+import java.util.List;
+
+/**
+ * Interface Intersectables is anything that can be intersected by a ray.
+ */
+public interface Intersectable {
+    /**
+     * This function takes in a ray and returns a list of points that the ray intersects
+     * @param ray
+     * @return A list of points which the ray intersects
+     */
+    List<Point> findIntsersections(Ray ray);
+}
