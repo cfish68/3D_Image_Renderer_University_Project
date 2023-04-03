212d85944d9e025a0cb682b9871a4df02dfb73ee
Chaim Fishman <csfwny@gmail.com>
2023-04-03 20:46:31 +0300

    completed all tests for plane intersection tests

--- unittests/geometries/SphereTests.java
+++ unittests/geometries/SphereTests.java
@@ -151,7 +151,7 @@ class SphereTests {
         assertEquals(0, result.size(),
                 "Wrong number of points. Expected intersections is 0 for ray which starts after the tangent point");
 // **** Group: Special cases
-// TC19: Ray's line is outside, ray is orthogonal to ray start to sphere's center line
+// TC22: Ray's line is outside, ray is orthogonal to ray start to sphere's center line
         result = sphere.findIntersections(new Ray(new Point(0,2,1), new Vector(0,0,1)));
         assertEquals(0, result.size(),
                 "Wrong number of points. Expected intersections is 0 for ray which starts after the tangent point");
