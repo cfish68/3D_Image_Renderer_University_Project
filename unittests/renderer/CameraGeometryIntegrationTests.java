3fa2ea8443714a27bf29e240c5eda9a88674e376
Avi-W <avi.weiner12@gmail.com>
2023-04-24 19:24:01 +0300

    Added in CameraGeometryIntegrationTests though confused about it

---
+++ unittests/renderer/CameraGeometryIntegrationTests.java
@@ -0,0 +1,9 @@
+package renderer;
+
+/**
+ * Check integration between creating rays from the camera and
+ * calculating intersections between rays and geometric bodies.
+ */
+public class CameraGeometryIntegrationTests {
+
+}
