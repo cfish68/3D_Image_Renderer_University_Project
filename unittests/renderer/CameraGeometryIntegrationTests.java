b0fd230965d915a1faa91f005ed659df9cba94aa
Chaim Fishman <csfwny@gmail.com>
2023-04-26 17:34:34 +0300

    fixed an error in test case 2 for sphere integrations

--- unittests/renderer/CameraGeometryIntegrationTests.java
+++ unittests/renderer/CameraGeometryIntegrationTests.java
@@ -42,21 +42,7 @@ public class CameraGeometryIntegrationTests {
     void CameraSphereTests() {
         Camera camera = new Camera(new Point(0, 0, 0), new Vector(0,-1,0),new Vector(0,0,1)).setVPDistance(1).setVPSize(3, 3);;
         Sphere sphere = new Sphere(1, new Point(0, -3, 0));
-        //the center of the viewPlane is (0,-1,0) (also the center of the middle
-        // pixel) the "top" is (0,-1,1.5) and the "right" is (1.5,-1,0) each pixel is a 1 by 1. the center of the top left pixel is\
-        //center of pixels
-        // [top row   [(-1,-1,1),(0,-1,1),(1,-1,1)],
-        // middle row[(-1,-1,0),(0,-1,0),(1,-1,0)],
-        // bottom row[(-1,-1,-1),(0,-1,-1),(1,-1,-1)]]
-//        ArrayList<Ray> rays = new ArrayList<Ray>();
-//        for(int i = 0; i < 3; i++){
-//            for(int j = 0; j < 3; j++){
-//                rays.add(camera.constructRay(3,3,j,i));
-//            }
-//
-//        }
 
-        //first test seems to only use the center pixel hence the above implementation (incomplete) has been abandoned and kept for further tests.
         //TC01: Sphere is after view plane expected is 2 intersection points
         assertEquals(2, sphere.findIntersections(camera.constructRay(3,3,1,1)).size(), "ERROR: TC01 failed simple case sphere after plane with one ray thru the middle of the plane");
 
