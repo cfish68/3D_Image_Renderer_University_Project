477caa5063a0b0b64c1b48b4e987c8dc8b356655
Avi-W <avi.weiner12@gmail.com>
2023-05-18 16:23:03 +0300

    diffusive works perfectly, commented out specular in the meantime

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -68,10 +68,10 @@ public class RayTracerBasic extends RayTraceBase {
             if (nl * nv > 0) { // sign(nl) == sing(nv)
                 Color iL = lightSource.getIntensity(gp.point);
                 //The light that gets diffused and scatters upon hitting the surface
-                color = color.add(iL.scale(calcDiffusive(material, nl)),
+                color = color.add(iL.scale(calcDiffusive(material, nl))
                         //the light that reflects more sharply and concisely
-                        iL.scale(Double3.ZERO));
-            } //calcSpecular(material, n, l, nl, v)
+                        ,iL.scale(calcSpecular(material, n, l, nl, v)));
+            }
         }
         return color;
     }
@@ -99,7 +99,7 @@ public class RayTracerBasic extends RayTraceBase {
      * @return
      */
     private Double3 calcSpecular(Material mat, Vector n, Vector l, double nl, Vector cameraDir){
-        Vector r = l.subtract(n.scale(nl).scale(2)); //reflectance vector
+        Vector r = l.subtract(n.scale(nl).scale(2)); //reflection vector
         return mat.kS
                 .scale(
                         Math.pow(Math.max(0,
